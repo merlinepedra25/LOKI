bc42382e726a077f008640f8743595d7654e9d75
Arve Knudsen <arve.knudsen@gmail.com>
2021-08-27 18:17:39 +0200

    Use kv from dskit
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/distributor/distributor_test.go
+++ pkg/distributor/distributor_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/cortexproject/cortex/pkg/ring"
 	ring_client "github.com/cortexproject/cortex/pkg/ring/client"
 	"github.com/cortexproject/cortex/pkg/util/test"
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 	"github.com/grafana/dskit/flagext"
 	"github.com/grafana/dskit/kv"
 	"github.com/grafana/dskit/kv/consul"
@@ -274,7 +274,9 @@ func TestDistributor_PushIngestionRateLimiter(t *testing.T) {
 
 			// Init a shared KVStore
 			kvStore, closer := consul.NewInMemoryClient(ring.GetCodec(), log.NewNopLogger())
-			t.Cleanup(func() { assert.NoError(t, closer.Close()) })
+			t.Cleanup(func() {
+				assert.NoError(t, closer.Close())
+			})
 
 			// Start all expected distributors
 			distributors := make([]*Distributor, testData.distributors)
